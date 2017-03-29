7f4dc5995d678656e1a688e9c766eac815222d90
brian <balsamo@uchicago.edu>
2017-03-29 15:22:21 -0500

    starting down a slightly different path from pypremis, with cleaner recursion up to whole record level.

--- qremis/__init__.py
+++ qremis/__init__.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from functools import partial
 
 
 class QremisRecord:
@@ -134,17 +134,109 @@ class QremisRecord:
     relationship_list = property(get_relationship_list, set_relationship_list, del_relationship_list)
 
 
-class QremisElement(metaclass=ABCMeta):
+def lowerFirst(s):
+    if not s:
+        return s
+    return s[0].lower() + s[1:]
+
+
+class QremisElement:
+    def __init__(self, *args, **kwargs):
+        # Be sure we can build a valid element
+        mandatory_fields = set(x for x in self._spec if self._spec[x]['mandatory'] is True)
+        provided_fields = set(lowerFirst(x.__class__.__name__) for x in args)
+        provided_fields = provided_fields.union(set([x for x in kwargs]))
+        if not mandatory_fields.issubset(provided_fields):
+            raise ValueError(
+                "The following are required for init, but were not present: {}".format(
+                    ", ".join(mandatory_fields - provided_fields)
+                )
+            )
+        for x in provided_fields:
+            if x not in self._spec:
+                raise TypeError("Erroneous field!")
+
+        # Dynamically build getters, setters, dellers, adders from spec
+        for x in self._spec:
+            setattr(self, "get_{}".format(x), partial(self.get_field, x))
+            setattr(self, "set_{}".format(x),
+                    partial(self.set_field, x, _type=self._spec[x]['type'],
+                            repeatable=self._spec[x]['repeatable']))
+            setattr(self, "del_{}".format(x), partial(self.del_field, x))
+            if self._spec[x]['repeatable']:
+                setattr(self, "add_{}".format(x), partial(self.add_to_field, x, _type=self._spec[x]['type']))
+
+            # Set the object properties
+            # TODO: Unbreak? Remove?
+            # I think this has something to do with attribute search order?
+            setattr(self, "{}".format(x), property(fget=getattr(self, "get_{}".format(x)),
+                                                   fset=getattr(self, "set_{}".format(x)),
+                                                   fdel=getattr(self, "del_{}".format(x))))
+
+        # Build the element with the init args
+        self._fields = {}
+        for x in args:
+            if not isinstance(x, QremisElement):
+                raise ValueError("Only QremisElement instance are accepted as args")
+            if self._spec[lowerFirst(x.__class__.__name__)]['repeatable']:
+                getattr(self, "add_{}".format(lowerFirst(x.__class__.__name__)))(x)
+            else:
+                getattr(self, "set_{}".format(lowerFirst(x.__class__.__name__)))(x)
+        for x in kwargs:
+            if self._spec[x]['repeatable']:
+                getattr(self, "add_{}".format(x))(kwargs[x])
+            else:
+                getattr(self, "set_{}".format(x))(kwargs[x])
+
+    def set_field(self, fieldname, fieldvalue, _type=None, repeatable=False):
+        if repeatable:
+            try:
+                self.add_to_field(fieldname, fieldvalue, _type=_type)
+            except TypeError:  # Maybe its an iter of values
+                for x in fieldvalue:
+                    self.add_to_field(fieldname, x, _type=_type)
+        else:
+            if _type is not None:
+                if not isinstance(fieldvalue, _type):
+                    raise TypeError()
+            self._fields[fieldname] = fieldvalue
+
+    def add_to_field(self, fieldname, fieldvalue, _type=None):
+        if _type is not None:
+            if not isinstance(fieldvalue, _type):
+                raise TypeError()
+        if fieldname not in self._fields:
+            self._fields[fieldname] = []
+        self._fields[fieldname].append(fieldvalue)
+
+    def get_field(self, fieldname):
+        return self._fields[fieldname]
+
+    def del_field(self, fieldname, index=None):
+        # Dynamically removes empty fields
+        if index:
+            del self._fields[fieldname][index]
+            if len(self._fields[fieldname]) == 0:
+                del self._fields[fieldname]
+        else:
+            del self._fields[fieldname]
+
     def to_dict(self):
-        pass
+        r = {}
+        return r
 
 
-class Qremis(QremisElement):
-    pass
+class ObjectIdentifier(QremisElement):
+    _spec = {
+        'objectIdentifierType': {'repeatable': False, 'mandatory': True, 'type': str},
+        'objectIdentifierValue': {'repeatable': False, 'mandatory': True, 'type': str}
+    }
 
 
 class Object(QremisElement):
-    pass
+    _spec = {
+        'objectIdentifier': {'repeatable': True, 'mandatory': True, 'type': ObjectIdentifier}
+    }
 
 
 class Event(QremisElement):
@@ -161,3 +253,13 @@ class Rights(QremisElement):
 
 class Relationship(QremisElement):
     pass
+
+
+class Qremis(QremisElement):
+    _spec = {
+        'object': {'repeatable': True, 'mandatory': False, 'type': Object},
+        'event': {'repeatable': True, 'mandatory': False, 'type': Event},
+        'agent': {'repeatable': True, 'mandatory': False, 'type': Agent},
+        'rights': {'repeatable': True, 'mandatory': False, 'type': Rights},
+        'relationship': {'repeatable': True, 'mandatory': False, 'type': Relationship}
+    }
